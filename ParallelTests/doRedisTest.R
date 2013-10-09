51c3d7dfb8278ed0d61a07392c42b12984aa321f
Randall Pruim <rpruim@calvin.edu>
2013-10-09 16:53:38 -0400

    initial parallel tests

---
+++ ParallelTests/doRedisTest.R
@@ -0,0 +1,23 @@
+
+require(doRedis)
+require(foreach)
+library('doRedis')
+registerDoRedis(queue='jobs')
+startLocalWorkers(n=50, queue='jobs')
+setChunkSize(1)
+print(getDoParWorkers())
+
+sleepy <- 1 + runif(400,-.5,.5) 
+
+etime <- system.time(
+x <- foreach( i = 1:200, .combine=c, .inorder=FALSE ) %dopar% {
+  Sys.sleep(sleepy[i])
+  i
+}
+)[3]
+removeQueue('jobs')
+print(xyplot( x ~ 1:length(x), 
+              main=paste("total:", sum(sleepy[1:length(x)])),
+              sub=paste("time:", etime)
+              ))
+print(etime)
