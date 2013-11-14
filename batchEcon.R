7c62f2db0cdd2ecdd1ed6d6c8187bb338ed46a9b
Randall Pruim <rpruim@gmail.com>
2013-11-13 22:51:10 -0500

    batchFile

---
+++ batchEcon.R
@@ -0,0 +1,34 @@
+#!/usr/bin/env Rscript --vanilla --default-packages=utils,stats,lattice,grid,mosaic,foreach,doParallel,plyr,xtable,nlmrt,micEconCES,systemfit,Matrix,lmtest,zoo,miscTools,micEcon,minpack.lm,DEoptim,iterators,parallel,latticeExtra,RColorBrewer,ggplot2,reshape2,scales
+
+suppressPackageStartupMessages(library("optparse"))
+
+option_list <- list(
+  make_option(c("-c", "--country"), default="US",
+              help="country [default=%default]"),
+  make_option(c("-e", "--energy"), default="Q",
+              help="energy [default=%default]"),
+  make_option(c("-m", "--model"), default="cese-(kl)e",
+              help="model [default=%default]"),
+  make_option(c("-n", "--resamples"), default=10L, type="integer",
+              help="number of resamples [default=%default]"),
+  make_option(c("-C", "--clobber"), default=FALSE, action="store_true",
+              help="number of resamples [default=%default]"),
+  make_option(c("-M", "--method"), default="wild", 
+              help="resampling method [default=%default]")
+  )
+
+opts <- parse_args(OptionParser(option_list=option_list))
+
+print(opts)
+
+source('Econ-Growth-Resampling.R')
+
+#genResampleData(modelType = "cese-(kl)e", countryAbbrev="CN", energyType="Q", n=2, method="wild", clobber=TRUE)
+#cat('half way')
+
+genResampleData(modelType=opts$model, countryAbbrev=opts$country, 
+                energyType=opts$energy, n=opts$resamples, method=opts$method, clobber=opts$clobber)
+
+
+
+  
