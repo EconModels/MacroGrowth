af1cdcc141438b5ea37503f4fcaaba419cb69cfd
MatthewHeun <matthew.heun@me.com>
2013-11-14 10:20:31 -0500

    Added single-factor models. Added start time.

--- batchEcon.R
+++ batchEcon.R
@@ -1,4 +1,9 @@
 #!/usr/bin/Rscript  --default-packages=utils,stats,lattice,grid,mosaic,methods,graphics,foreach,doParallel,plyr,xtable,nlmrt,micEconCES,systemfit,Matrix,lmtest,zoo,miscTools,micEcon,minpack.lm,DEoptim,iterators,parallel,latticeExtra,RColorBrewer,ggplot2,reshape2,scales
+
+cat("\nStart @ ")
+cat(date())
+cat('\n\n')
+
 source('Econ-Growth-Resampling.R',echo=FALSE,verbose=FALSE)
 suppressPackageStartupMessages(library("optparse"))
 
@@ -8,6 +13,8 @@ option_list <- list(
               help="country [default=%default]"),
   make_option(c("-e", "--energy"), default="Q",
               help="energy [default=%default]"),
+  make_option(c("-f", "--factor"), default="K",
+              help="factor [default=%default]"),
   make_option(c("-m", "--model"), default="all",
               help="model [default=%default]"),
   make_option(c("-n", "--resamples"), default=10L, type="integer",
@@ -43,6 +50,12 @@ if(opts$energy== "all") {
   opts$energy <- match.arg(opts$energy,choices=energyTypes,several.ok=TRUE)
 }
 
+if(opts$factor== "all") {
+  opts$factor <- factors
+} else {
+  opts$factor <- toupper(strsplit(opts$factor,",")[[1]])
+  opts$factor <- match.arg(opts$factor,choices=factors,several.ok=TRUE)
+}
 
 print(str(opts))
 
@@ -53,17 +66,21 @@ if( ! opts$debug) {
   #genResampleData(modelType = "cese-(kl)e", countryAbbrev="CN", energyType="Q", n=2, method="wild", clobber=TRUE)
   #cat('half way')
 
-  for (country in opts$country)
-    for (model in opts$model)
+  for (country in opts$country) {
+    for (model in opts$model) {
       for (energy in opts$energy) {
         cat(paste0("\nFitting ", country, ":", model, ":",energy))
         genResampleData(modelType=model, 
                         countryAbbrev=country,  
                         energyType=energy, 
+                        factor=factor,
                         n=opts$resamples, 
                         method=opts$method, 
                         clobber=opts$clobber)
+        
       }
+    }
+  }
   cat("\nDone @ ")
   cat(date())
   cat('\n\n')
