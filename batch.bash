e4db8131e410fe4ff65078d672ffbf68233d4d45
Randall Pruim <rpruim@gmail.com>
2013-11-16 09:28:21 -0500

    commandline arguments (for -C and -d, for example)

--- batch.bash
+++ batch.bash
@@ -11,38 +11,38 @@
 LOC_PATH=`pwd`
 echo $LOC_PATH
 
-ssh node-01 "cd $LOC_PATH; ./batchEcon.R -c US,UK -e Q -m cese-\(kl\)e -n 1000 -C > node-01.txt" &
-ssh node-02 "cd $LOC_PATH; ./batchEcon.R -c JP,CN -e Q -m cese-\(kl\)e -n 1000 -C > node-02.txt" &
-ssh node-03 "cd $LOC_PATH; ./batchEcon.R -c ZA,SA -e Q -m cese-\(kl\)e -n 1000 -C > node-03.txt" &
-ssh node-04 "cd $LOC_PATH; ./batchEcon.R -c IR,TZ -e Q -m cese-\(kl\)e -n 1000 -C > node-04.txt" &
-ssh node-05 "cd $LOC_PATH; ./batchEcon.R -c ZM    -e Q -m cese-\(kl\)e -n 1000 -C > node-05.txt" &
-ssh node-06 "cd $LOC_PATH; ./batchEcon.R -c US,UK -e Q -m cese-\(le\)k -n 1000 -C > node-06.txt" &
-ssh node-07 "cd $LOC_PATH; ./batchEcon.R -c JP,CN -e Q -m cese-\(le\)k -n 1000 -C > node-07.txt" &
-ssh node-09 "cd $LOC_PATH; ./batchEcon.R -c ZA,SA -e Q -m cese-\(le\)k -n 1000 -C > node-09.txt" &
-ssh node-10 "cd $LOC_PATH; ./batchEcon.R -c IR,TZ -e Q -m cese-\(le\)k -n 1000 -C > node-10.txt" &
-ssh node-12 "cd $LOC_PATH; ./batchEcon.R -c ZM    -e Q -m cese-\(le\)k -n 1000 -C > node-12.txt" &
-ssh node-13 "cd $LOC_PATH; ./batchEcon.R -c US,UK -e Q -m cese-\(ek\)l -n 1000 -C > node-13.txt" &
-ssh node-14 "cd $LOC_PATH; ./batchEcon.R -c JP,CN -e Q -m cese-\(ek\)l -n 1000 -C > node-14.txt" & 
-ssh node-15 "cd $LOC_PATH; ./batchEcon.R -c ZA,SA -e Q -m cese-\(ek\)l -n 1000 -C > node-15.txt" &
-ssh node-16 "cd $LOC_PATH; ./batchEcon.R -c IR,TZ -e Q -m cese-\(ek\)l -n 1000 -C > node-16.txt" &
-ssh node-17 "cd $LOC_PATH; ./batchEcon.R -c ZM    -e Q -m cese-\(ek\)l -n 1000 -C > node-17.txt" &
+ssh node-01 "cd $LOC_PATH; ./batchEcon.R -c US,UK -e Q -m cese-\(kl\)e -n 1000 $1 > node-01.txt" &
+ssh node-02 "cd $LOC_PATH; ./batchEcon.R -c JP,CN -e Q -m cese-\(kl\)e -n 1000 $1 > node-02.txt" &
+ssh node-03 "cd $LOC_PATH; ./batchEcon.R -c ZA,SA -e Q -m cese-\(kl\)e -n 1000 $1 > node-03.txt" &
+ssh node-04 "cd $LOC_PATH; ./batchEcon.R -c IR,TZ -e Q -m cese-\(kl\)e -n 1000 $1 > node-04.txt" &
+ssh node-05 "cd $LOC_PATH; ./batchEcon.R -c ZM    -e Q -m cese-\(kl\)e -n 1000 $1 > node-05.txt" &
+ssh node-06 "cd $LOC_PATH; ./batchEcon.R -c US,UK -e Q -m cese-\(le\)k -n 1000 $1 > node-06.txt" &
+ssh node-07 "cd $LOC_PATH; ./batchEcon.R -c JP,CN -e Q -m cese-\(le\)k -n 1000 $1 > node-07.txt" &
+ssh node-09 "cd $LOC_PATH; ./batchEcon.R -c ZA,SA -e Q -m cese-\(le\)k -n 1000 $1 > node-09.txt" &
+ssh node-10 "cd $LOC_PATH; ./batchEcon.R -c IR,TZ -e Q -m cese-\(le\)k -n 1000 $1 > node-10.txt" &
+ssh node-12 "cd $LOC_PATH; ./batchEcon.R -c ZM    -e Q -m cese-\(le\)k -n 1000 $1 > node-12.txt" &
+ssh node-13 "cd $LOC_PATH; ./batchEcon.R -c US,UK -e Q -m cese-\(ek\)l -n 1000 $1 > node-13.txt" &
+ssh node-14 "cd $LOC_PATH; ./batchEcon.R -c JP,CN -e Q -m cese-\(ek\)l -n 1000 $1 > node-14.txt" & 
+ssh node-15 "cd $LOC_PATH; ./batchEcon.R -c ZA,SA -e Q -m cese-\(ek\)l -n 1000 $1 > node-15.txt" &
+ssh node-16 "cd $LOC_PATH; ./batchEcon.R -c IR,TZ -e Q -m cese-\(ek\)l -n 1000 $1 > node-16.txt" &
+ssh node-17 "cd $LOC_PATH; ./batchEcon.R -c ZM    -e Q -m cese-\(ek\)l -n 1000 $1 > node-17.txt" &
 
 # These models take hardly any time, so we'll run them all on their own node. 
 # The next line runs all countires and all energy types for all models except CES with energy.
-ssh node-18 "cd $LOC_PATH; ./batchEcon.R -c all -e all -f all -m sf,cd,cde,ces,linex -n 1000 -C > node-18.txt" &
+ssh node-18 "cd $LOC_PATH; ./batchEcon.R -c all -e all -f all -m sf,cd,cde,ces,linex -n 1000 $1 > node-18.txt" &
 
 # The next lines run the analyses for exergy (X) and useful work (U) energy types 
 # for the CES models with energy.
-ssh node-19 "cd $LOC_PATH; ./batchEcon.R -c US,UK,JP -e X -m cese-\(kl\)e -n 1000 -C > node-19.txt" &
-ssh node-20 "cd $LOC_PATH; ./batchEcon.R -c CN,ZA,SA -e X -m cese-\(kl\)e -n 1000 -C > node-20.txt" &
-ssh node-21 "cd $LOC_PATH; ./batchEcon.R -c IR,TZ,ZM -e X -m cese-\(kl\)e -n 1000 -C > node-21.txt" &
-ssh node-22 "cd $LOC_PATH; ./batchEcon.R -c US,UK,JP -e X -m cese-\(le\)k -n 1000 -C > node-22.txt" &
-ssh node-23 "cd $LOC_PATH; ./batchEcon.R -c CN,ZA,SA -e X -m cese-\(le\)k -n 1000 -C > node-23.txt" &
-ssh node-24 "cd $LOC_PATH; ./batchEcon.R -c IR,TZ,ZM -e X -m cese-\(le\)k -n 1000 -C > node-24.txt" &
-ssh node-25 "cd $LOC_PATH; ./batchEcon.R -c US,UK,JP -e X -m cese-\(ek\)l -n 1000 -C > node-25.txt" &
-ssh node-26 "cd $LOC_PATH; ./batchEcon.R -c CN,ZA,SA -e X -m cese-\(ek\)l -n 1000 -C > node-26.txt" &
-ssh node-27 "cd $LOC_PATH; ./batchEcon.R -c IR,TZ,ZM -e X -m cese-\(ek\)l -n 1000 -C > node-27.txt" &
+ssh node-19 "cd $LOC_PATH; ./batchEcon.R -c US,UK,JP -e X -m cese-\(kl\)e -n 1000 $1 > node-19.txt" &
+ssh node-20 "cd $LOC_PATH; ./batchEcon.R -c CN,ZA,SA -e X -m cese-\(kl\)e -n 1000 $1 > node-20.txt" &
+ssh node-21 "cd $LOC_PATH; ./batchEcon.R -c IR,TZ,ZM -e X -m cese-\(kl\)e -n 1000 $1 > node-21.txt" &
+ssh node-22 "cd $LOC_PATH; ./batchEcon.R -c US,UK,JP -e X -m cese-\(le\)k -n 1000 $1 > node-22.txt" &
+ssh node-23 "cd $LOC_PATH; ./batchEcon.R -c CN,ZA,SA -e X -m cese-\(le\)k -n 1000 $1 > node-23.txt" &
+ssh node-24 "cd $LOC_PATH; ./batchEcon.R -c IR,TZ,ZM -e X -m cese-\(le\)k -n 1000 $1 > node-24.txt" &
+ssh node-25 "cd $LOC_PATH; ./batchEcon.R -c US,UK,JP -e X -m cese-\(ek\)l -n 1000 $1 > node-25.txt" &
+ssh node-26 "cd $LOC_PATH; ./batchEcon.R -c CN,ZA,SA -e X -m cese-\(ek\)l -n 1000 $1 > node-26.txt" &
+ssh node-27 "cd $LOC_PATH; ./batchEcon.R -c IR,TZ,ZM -e X -m cese-\(ek\)l -n 1000 $1 > node-27.txt" &
 # We have useful work data for US, UK, and JP only.
-ssh node-28 "cd $LOC_PATH; ./batchEcon.R -c US,UK,JP -e U -m cese-\(kl\)e -n 1000 -C > node-28.txt" &
-ssh node-29 "cd $LOC_PATH; ./batchEcon.R -c US,UK,JP -e U -m cese-\(le\)k -n 1000 -C > node-29.txt" &
-ssh node-30 "cd $LOC_PATH; ./batchEcon.R -c US,UK,JP -e U -m cese-\(ek\)l -n 1000 -C > node-30.txt" &
+ssh node-28 "cd $LOC_PATH; ./batchEcon.R -c US,UK,JP -e U -m cese-\(kl\)e -n 1000 $1 > node-28.txt" &
+ssh node-29 "cd $LOC_PATH; ./batchEcon.R -c US,UK,JP -e U -m cese-\(le\)k -n 1000 $1 > node-29.txt" &
+ssh node-30 "cd $LOC_PATH; ./batchEcon.R -c US,UK,JP -e U -m cese-\(ek\)l -n 1000 $1 > node-30.txt" &
