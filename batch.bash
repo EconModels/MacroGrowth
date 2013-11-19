b1960bd8ca6fb5f57c1ff5a732cadba8c61048ff
MatthewHeun <matthew.heun@me.com>
2013-11-19 10:19:08 -0500

    Now avoiding node-06.

--- batch.bash
+++ batch.bash
@@ -32,30 +32,31 @@ ssh node-02 "cd $LOC_PATH; ./batchEcon.R -c US,UK,JP -e Q -m cese-\(kl\)e $1 &> 
 ssh node-03 "cd $LOC_PATH; ./batchEcon.R -c CN,ZA,SA -e Q -m cese-\(kl\)e $1 &> data_resample/node-03.txt" &
 ssh node-04 "cd $LOC_PATH; ./batchEcon.R -c IR,TZ,ZM -e Q -m cese-\(kl\)e $1 &> data_resample/node-04.txt" &
 ssh node-05 "cd $LOC_PATH; ./batchEcon.R -c US,UK,JP -e Q -m cese-\(le\)k $1 &> data_resample/node-05.txt" &
-ssh node-06 "cd $LOC_PATH; ./batchEcon.R -c CN,ZA,SA -e Q -m cese-\(le\)k $1 &> data_resample/node-06.txt" &
-ssh node-07 "cd $LOC_PATH; ./batchEcon.R -c IR,TZ,ZM -e Q -m cese-\(le\)k $1 &> data_resample/node-07.txt" &
+# node-06 has only 4 working processors.  We can access only 2 of those.  So, best to avoid node-06 for now.
+ssh node-07 "cd $LOC_PATH; ./batchEcon.R -c CN,ZA,SA -e Q -m cese-\(le\)k $1 &> data_resample/node-07.txt" &
+ssh node-09 "cd $LOC_PATH; ./batchEcon.R -c IR,TZ,ZM -e Q -m cese-\(le\)k $1 &> data_resample/node-09.txt" &
 # Gary Draving suggested avoiding node-08, because of I/O issues.
-ssh node-09 "cd $LOC_PATH; ./batchEcon.R -c US,UK,JP -e Q -m cese-\(ek\)l $1 &> data_resample/node-09.txt" &
-ssh node-10 "cd $LOC_PATH; ./batchEcon.R -c CN,ZA,SA -e Q -m cese-\(ek\)l $1 &> data_resample/node-10.txt" & 
+ssh node-10 "cd $LOC_PATH; ./batchEcon.R -c US,UK,JP -e Q -m cese-\(ek\)l $1 &> data_resample/node-10.txt" &
+ssh node-12 "cd $LOC_PATH; ./batchEcon.R -c CN,ZA,SA -e Q -m cese-\(ek\)l $1 &> data_resample/node-12.txt" & 
 # node-11 is not functional.
-ssh node-12 "cd $LOC_PATH; ./batchEcon.R -c IR,TZ,ZM -e Q -m cese-\(ek\)l $1 &> data_resample/node-12.txt" &
+ssh node-13 "cd $LOC_PATH; ./batchEcon.R -c IR,TZ,ZM -e Q -m cese-\(ek\)l $1 &> data_resample/node-13.txt" &
 
 # The next lines run the cese analyses for exergy (X)
 
-ssh node-13 "cd $LOC_PATH; ./batchEcon.R -c US,UK,JP -e X -m cese-\(kl\)e $1 &> data_resample/node-13.txt" &
-ssh node-14 "cd $LOC_PATH; ./batchEcon.R -c CN,ZA,SA -e X -m cese-\(kl\)e $1 &> data_resample/node-14.txt" &
-ssh node-15 "cd $LOC_PATH; ./batchEcon.R -c IR,TZ,ZM -e X -m cese-\(kl\)e $1 &> data_resample/node-15.txt" &
-ssh node-16 "cd $LOC_PATH; ./batchEcon.R -c US,UK,JP -e X -m cese-\(le\)k $1 &> data_resample/node-16.txt" &
-ssh node-17 "cd $LOC_PATH; ./batchEcon.R -c CN,ZA,SA -e X -m cese-\(le\)k $1 &> data_resample/node-17.txt" &
-ssh node-18 "cd $LOC_PATH; ./batchEcon.R -c IR,TZ,ZM -e X -m cese-\(le\)k $1 &> data_resample/node-18.txt" &
-ssh node-19 "cd $LOC_PATH; ./batchEcon.R -c US,UK,JP -e X -m cese-\(ek\)l $1 &> data_resample/node-19.txt" &
-ssh node-20 "cd $LOC_PATH; ./batchEcon.R -c CN,ZA,SA -e X -m cese-\(ek\)l $1 &> data_resample/node-20.txt" &
+ssh node-14 "cd $LOC_PATH; ./batchEcon.R -c US,UK,JP -e X -m cese-\(kl\)e $1 &> data_resample/node-14.txt" &
+ssh node-15 "cd $LOC_PATH; ./batchEcon.R -c CN,ZA,SA -e X -m cese-\(kl\)e $1 &> data_resample/node-15.txt" &
+ssh node-16 "cd $LOC_PATH; ./batchEcon.R -c IR,TZ,ZM -e X -m cese-\(kl\)e $1 &> data_resample/node-16.txt" &
+ssh node-17 "cd $LOC_PATH; ./batchEcon.R -c US,UK,JP -e X -m cese-\(le\)k $1 &> data_resample/node-17.txt" &
+ssh node-18 "cd $LOC_PATH; ./batchEcon.R -c CN,ZA,SA -e X -m cese-\(le\)k $1 &> data_resample/node-18.txt" &
+ssh node-19 "cd $LOC_PATH; ./batchEcon.R -c IR,TZ,ZM -e X -m cese-\(le\)k $1 &> data_resample/node-19.txt" &
+ssh node-20 "cd $LOC_PATH; ./batchEcon.R -c US,UK,JP -e X -m cese-\(ek\)l $1 &> data_resample/node-20.txt" &
 # Faulty R installations on nodes 21, 22, 23, and 24.
-ssh node-25 "cd $LOC_PATH; ./batchEcon.R -c IR,TZ,ZM -e X -m cese-\(ek\)l $1 &> data_resample/node-26.txt" &
+ssh node-25 "cd $LOC_PATH; ./batchEcon.R -c CN,ZA,SA -e X -m cese-\(ek\)l $1 &> data_resample/node-25.txt" &
+# Faulty R installation on node 26.
+ssh node-27 "cd $LOC_PATH; ./batchEcon.R -c IR,TZ,ZM -e X -m cese-\(ek\)l $1 &> data_resample/node-27.txt" &
 
 # We have useful work (U) data for US, UK, and JP only.
 
-# Faulty R installation on node 26.
-ssh node-27 "cd $LOC_PATH; ./batchEcon.R -c US,UK,JP -e U -m cese-\(kl\)e $1 &> data_resample/node-27.txt" &
-ssh node-28 "cd $LOC_PATH; ./batchEcon.R -c US,UK,JP -e U -m cese-\(le\)k $1 &> data_resample/node-28.txt" &
-ssh node-29 "cd $LOC_PATH; ./batchEcon.R -c US,UK,JP -e U -m cese-\(ek\)l $1 &> data_resample/node-29.txt" &
+ssh node-28 "cd $LOC_PATH; ./batchEcon.R -c US,UK,JP -e U -m cese-\(kl\)e $1 &> data_resample/node-28.txt" &
+ssh node-29 "cd $LOC_PATH; ./batchEcon.R -c US,UK,JP -e U -m cese-\(le\)k $1 &> data_resample/node-29.txt" &
+ssh node-30 "cd $LOC_PATH; ./batchEcon.R -c US,UK,JP -e U -m cese-\(ek\)l $1 &> data_resample/node-30.txt" &
