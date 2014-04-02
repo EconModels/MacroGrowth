895d321251fb42c13b9ba485833938447eac0b2c
Randall Pruim <rpruim@calvin.edu>
2014-04-02 15:19:16 -0400

    some tests of refactored fitting functions

---
+++ Testing/RefactorTesting.R
@@ -0,0 +1,39 @@
+require(EconModels2)
+
+all <- read.table("data/AllData.txt", header=TRUE)
+
+naturalCoef(
+  cdModel2( response = iGDP, capital=iCapStk, labor=iLabor, time=iYear, data=subset(all, Country=="US") )
+)
+
+naturalCoef(
+  cdModel2( iGDP ~ iCapStk + iLabor + iYear, data=subset(all, Country=="US") )
+)
+
+naturalCoef(
+  cdeModel2( response = iGDP, capital=iCapStk, labor=iLabor, energy=iQ, time=iYear, data=subset(all, Country=="US") )
+)
+
+naturalCoef(
+  cdeModel2( iGDP ~ iCapStk + iLabor + iQ + iYear, data=subset(all, Country=="US") )
+)
+
+naturalCoef(
+  linexModel2( iGDP ~ iCapStk + iLabor + iQ + iYear, data=subset(all, Country=="US") )
+)
+
+naturalCoef(
+  linexModel2( response=iGDP, capital= iCapStk, labor=iLabor, energy=iQ, time= iYear, 
+               data=subset(all, Country=="US") )
+)
+naturalCoef(
+  singleFactorModel2( iGDP ~ iCapStk + iYear, data=subset(all, Country=="US") )
+)
+
+naturalCoef(
+  singleFactorModel2( response=iGDP, factor=iCapStk, time=iYear, data=subset(all, Country=="US") )
+)
+
+naturalCoef(
+  singleFactorModel2( iGDP ~ iCapStk + iYear, data=subset(all, Country=="US"), constrained=TRUE )
+)
