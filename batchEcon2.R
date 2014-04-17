7c6cff0c5136ceb004ecff08c8ca9a2f02aceed3
MatthewHeun <matthew.heun@me.com>
2014-04-17 16:08:54 -0400

    In the middle of working on batchEcon2.R. It is not working yet. But, making progress.

---
+++ batchEcon2.R
@@ -0,0 +1,328 @@
+#!/usr/bin/Rscript  --default-packages=utils,stats,lattice,grid,mosaic,methods,graphics,foreach,doParallel,plyr,xtable,nlmrt,micEconCES,systemfit,Matrix,lmtest,zoo,miscTools,micEcon,minpack.lm,DEoptim,iterators,parallel,latticeExtra,RColorBrewer,ggplot2,reshape2,scales
+
+# Example usage:
+
+# Move into the top level directory that contains this file
+# for US, thermal energy, single factor model, 10 resamples, clobbering previous results, and wild resampling:
+# ./batchEcon.R -c US -f Q -m sf -n 10 -C -M wild -H data -R data_resample
+# for US, exergy, linex model, 10 resamples, clobbering previous results, and wild resampling:
+# ./batchEcon.R -c US -e X -m linex -n 10 -C -M wild -H data -R data_resample
+# for all countries, all energy types, all models, 1000 resamples, clobber previous results, wild resampling:
+# ./batchEcon.R -c all -e all -m all -n 1000 -C -M wild -H data -R data_resample
+
+# print(sort(.packages()))
+require(EconModels2)
+suppressPackageStartupMessages(library("optparse"))
+
+# Some specifics for our data sets and analyses
+countryAbbrevs <- c(US="US", UK="UK", JP="JP", CN="CN", ZA="ZA", SA="SA", IR="IR", TZ="TZ", ZM="ZM")
+modelTypes <- c('sf', 'cd', 'cde', 'ces', 'cese-(kl)e', 'cese-(le)k', 'cese-(ek)l', 'linex')
+energyTypes <- c(Q="iQ", X="iX", U="iU") 
+factors <- c(K="iCapStk", L="iLabor", energyTypes) 
+
+
+#' Path to resample data file
+#' 
+#' This function returns a string representing the relative file path for the resample data being requested.
+#' 
+#' @param modelType one of \code{"sf"}, \code{"cd"}, \code{"cde"}, \code{"cese-(kl)"}, 
+#' \code{"cese-(kl)e"}, \code{"cese-(le)k"}, \code{"cese-(kl)e"}, or \code{"linex"}.
+#' @param countryAbbrev, a character string naming the country, 
+#' if you want to use original data.
+#' @param energyType one of \code{"Q"} (for thermal energy), \code{"X"} (for exergy), or \code{"U"} (for useful work).
+#' @param factor one of \code{"K"} (for capital stock), \code{"L"} (for labor), \code{"Q"} (for thermal energy), 
+#' \code{"X"} (for exergy), or \code{"U"} (for useful work).
+#' @param baseResample the relative path of the top-level directory containing the resample data.
+#' @return the relative path of the file containing the data for the requested resample data.
+getPathForResampleData <- function(modelType, countryAbbrev, energyType="none", factor="K", baseResample){
+  return(doGetPath(prefix="resampleData", modelType=modelType, countryAbbrev=countryAbbrev, 
+                   energyType=energyType, factor=factor, baseResample=baseResample))
+}
+
+#' Path to resample models file
+#' 
+#' This function returns a string representing the relative file path for the resample models being requested.
+#' 
+#' @param modelType one of \code{"sf"}, \code{"cd"}, \code{"cde"}, \code{"cese-(kl)"}, 
+#' \code{"cese-(kl)e"}, \code{"cese-(le)k"}, \code{"cese-(kl)e"}, or \code{"linex"}.
+#' @param countryAbbrev, a character string naming the country, 
+#' if you want to use original data.
+#' @param energyType one of \code{"Q"} (for thermal energy), \code{"X"} (for exergy), or \code{"U"} (for useful work).
+#' @param factor one of \code{"K"} (for capital stock), \code{"L"} (for labor), \code{"Q"} (for thermal energy), 
+#' \code{"X"} (for exergy), or \code{"U"} (for useful work).
+#' @param baseResample the relative path of the top-level directory containing the resample data.
+#' @return the relative path of the file containing the data for the requested resample data.
+getPathForResampleModels <- function(modelType, countryAbbrev, energyType="none", factor="K", baseResample){
+  return(doGetPath(prefix="resampleModels", modelType=modelType, countryAbbrev=countryAbbrev, 
+                   energyType=energyType, factor=factor, baseResample=baseResample))
+}
+
+#' Generates paths to resample coefficients or models in String format
+#' 
+#' This function returns a string representing the relative file path for the resample coefficients or models being requested.
+#' 
+#' @param prefix the prefix for the model file names. 
+#' @param modelType one of \code{"sf"}, \code{"cd"}, \code{"cde"}, \code{"cese-(kl)"}, 
+#' \code{"cese-(kl)e"}, \code{"cese-(le)k"}, \code{"cese-(kl)e"}, or \code{"linex"}.
+#' @param countryAbbrev, a character string naming the country, 
+#' if you want to use original data.
+#' @param energyType one of \code{"Q"} (for thermal energy), \code{"X"} (for exergy), or \code{"U"} (for useful work).
+#' @param factor one of \code{"K"} (for capital stock), \code{"L"} (for labor), \code{"Q"} (for thermal energy), 
+#' \code{"X"} (for exergy), or \code{"U"} (for useful work).
+#' @param baseResample the relative path of the top-level directory containing the resample data.
+#' @return the relative path of the file containing the data for the requested resample data.
+doGetPath <- function(prefix, modelType, countryAbbrev, energyType="Q", factor="K", baseResample){
+  if (energyType == "none"){
+    energyType="NA"
+  }
+  folder <- getFolderForResampleData(modelType=modelType, countryAbbrev=countryAbbrev, baseResample=baseResample)   
+  rdat <- ".Rdata"
+  filename <- switch(modelType,
+                     "sf"         = paste(prefix, "-", modelType, "-", countryAbbrev, "-", factor,     rdat, sep=""),
+                     "cd"         = paste(prefix, "-", modelType, "-", countryAbbrev, "-", "NA",       rdat, sep=""),
+                     "cde"        = paste(prefix, "-", modelType, "-", countryAbbrev, "-", energyType, rdat, sep=""),
+                     "ces"        = paste(prefix, "-", modelType, "-", countryAbbrev, "-", "NA",       rdat, sep=""),
+                     "cese-(kl)"  = paste(prefix, "-", "ces",     "-", countryAbbrev, "-", "NA",       rdat, sep=""),
+                     "cese-(kl)e" = paste(prefix, "-", modelType, "-", countryAbbrev, "-", energyType, rdat, sep=""),
+                     "cese-(le)k" = paste(prefix, "-", modelType, "-", countryAbbrev, "-", energyType, rdat, sep=""),
+                     "cese-(ek)l" = paste(prefix, "-", modelType, "-", countryAbbrev, "-", energyType, rdat, sep=""),
+                     "linex"      = paste(prefix, "-", modelType, "-", countryAbbrev, "-", energyType, rdat, sep=""),
+                     stop(paste("Unknown modelType", modelType, "in getPathForResampleModels."))
+  )
+  path <- file.path(folder, filename)
+  return(path)
+}
+
+#' Directory for resample data
+#' 
+#' This function returns a string representing the relative directory path containing the resample data being requested.
+#' 
+#' @param modelType one of \code{"sf"}, \code{"cd"}, \code{"cde"}, \code{"cese-(kl)"}, 
+#' \code{"cese-(kl)e"}, \code{"cese-(le)k"}, \code{"cese-(kl)e"}, or \code{"linex"}.
+#' @param countryAbbrev, a character string naming the country, 
+#' if you want to use original data.
+#' @param baseResample the relative path of the top-level directory containing the resample data.
+#' @return the relative path to the directory containing the data for the requested resample data.
+getFolderForResampleData <- function(modelType=modelTypes, countryAbbrev=countryAbbrevs, baseResample){
+  dr <- baseResample
+#   countryAbbrev <- match.arg(countryAbbrev)
+  folder <- switch(modelType,
+                   "cde"       = file.path(dr, "cd",      countryAbbrev),
+                   "cese-(kl)" = file.path(dr, "ces",     countryAbbrev),
+                   file.path(dr, modelType, countryAbbrev)
+  )
+  return(folder)
+}
+  
+# 
+# Start the script
+# 
+
+option_list <- list(
+  make_option(c("-c", "--country"), default="all",
+              help="country [default=%default]"),
+  make_option(c("-e", "--energy"), default="Q",
+              help="energy [default=%default]"),
+  make_option(c("-f", "--factor"), default="K",
+              help="factor [default=%default]"),
+  make_option(c("-m", "--model"), default="all",
+              help="model [default=%default]"),
+  make_option(c("-n", "--resamples"), default=10L, type="integer",
+              help="number of resamples [default=%default]"),
+  make_option(c("-C", "--clobber"), default=FALSE, action="store_true",
+              help="Clobber all previous files [default=%default]"),
+  make_option(c("-d", "--debug"), default=FALSE, action="store_true",
+              help="runs without executing the resampling [default=%default]"),
+  make_option(c("-M", "--method"), default="wild", 
+              help="resampling method [default=%default]"),
+  make_option(c("-H", "--baseHistorical"), # default="data", 
+              help="relative path to directory for historical data"),
+  make_option(c("-R", "--baseResample"), # default="data_resample", 
+              help="relative path to directory for resample data")
+)
+
+opts <- parse_args(OptionParser(option_list=option_list))
+# print(opts)
+
+if(opts$model == "all") {
+  opts$model <- modelTypes
+} else if (opts$model == "fast") {
+  opts$model <- setdiff(modelTypes, c("cese-(kl)e", "cese-(le)k", "cese-(ek)l"))
+  } else {
+  opts$model <- strsplit(opts$model,",")[[1]]
+#   opts$model <- match.arg(opts$model,choices=modelTypes,several.ok=TRUE)
+}
+
+if(opts$country== "all") {
+  opts$country <- countryAbbrevs
+} else {
+  opts$country <- strsplit(opts$country,",")[[1]]
+#   opts$country <- match.arg(opts$country,choices=countryAbbrevs,several.ok=TRUE)
+}
+
+if(opts$energy== "all") {
+  opts$energy <- energyTypes
+} else {
+  opts$energy <- toupper(strsplit(opts$energy,",")[[1]])
+#   opts$energy <- match.arg(opts$energy,choices=energyTypes,several.ok=TRUE)
+}
+
+if(opts$factor== "all") {
+  opts$factor <- factors
+} else {
+  opts$factor <- toupper(strsplit(opts$factor,",")[[1]])
+#   opts$factor <- match.arg(opts$factor,choices=factors,several.ok=TRUE)
+}
+
+print(str(opts))
+
+#
+# Convert the options to a ModelInfos object. This object contains the 
+# recipe for which fits to execute.
+#
+
+ModelInfos <- list()
+
+for(model in opts$model){
+  if (model == "sf"){
+    # Build a formula for each factor
+    formulas <- list()
+    for(factor in opts$factors){
+      formulas[[length(formulas)+1]] <- paste("iGDP ~", factor, "+ iYear")
+    }
+    ModelInfos[[length(ModelInfos)+1]] <- list(formulaStr = formulas,
+                                               fun = "singleFactorModel",
+                                               n=opts$n,
+                                               dots = list())
+  } else if (model == "cd") {
+    formulas <- c("iGDP ~ iCapStk + iLabor + iYear",
+                  "iGDP ~ iCapStk + iLabor + energy + iYear")
+  }
+  
+}
+print(ModelInfos)
+stop()
+
+startTime <- proc.time()
+cat("\n\nStart @ ")
+cat(date())
+cat('\n')
+
+if( ! opts$debug) {
+  
+  #genResampleData(modelType = "cese-(kl)e", countryAbbrev="CN", energyType="Q", n=2, method="wild", clobber=TRUE)
+  #cat('half way')
+  
+  # load historical data
+  All <- read.table(file="data/AllData.txt", header=TRUE)
+  
+  registerDoParallel()
+  
+  for (m in ModelInfos) {
+    for (f in m$formulaStr) {
+      for (energy in if (grepl("energy", f))  Energies else 'noEnergy') {
+        for (country in Countries) {
+          cdata <- subset(All, Country==country)
+          
+          formulaStr <- sub( "energy", energy, f ) 
+          formula <- eval( parse( text= formulaStr ) )
+          # formula <- substitute( iGDP ~ iCapStk + iLabor + e + iYear, list(e = energy))
+          # tryCatch to skip over country/energy combos that don't exist.
+          cat ( paste(country, formulaStr, m$fun, m$dots, m$n, sep=" : ") )
+          cat ("\n")
+          
+          tryCatch({
+            oModel <- do.call( m$fun, c( list( formula, data=cdata ), m$dots) )
+            oModels[[length(oModels) + 1]] <- oModel
+            #           Here is some sample code to pass prevModel=oModel for cesModel only. But, it doesn't work.
+            if (m$fun == "cesModel") {
+              # Want to set prevModel to oModel in the call to cesModel. It will be passed in the ... argument.
+              rFits <- resampledFits( oModel, "wild", n=m$n, id=paste(country,energy,m$fun, sep=":"), prevModel=oModel )
+            } else {
+              # No need for a prevModel argument, because none of the model functions (except cesModel) use it.
+              rFits <- resampledFits( oModel, "wild", n=m$n, id=paste(country,energy,m$fun, sep=":") )
+            }
+            rFits <- resampledFits( oModel, "wild", n=m$n, id=paste(country,energy,m$fun, sep=":") )
+            rModels[[length(rModels) + 1]] <- rFits[["models"]]
+            coefs[[length(coefs) + 1]] <- rFits[["coeffs"]]
+          }, 
+          error=function(e) {
+            cat(paste0("  *** Skipping ", energy, " for ", country, "\n"))
+            print(e)
+          }
+          )
+        }
+      }
+    }
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+#   for (model in opts$model) {
+#     if (model == "sf"){
+#       for (factor in opts$factor){
+#         # Working on the single-factor model. So, we need to pass the factor argument.
+#         foreach(country=opts$country, .errorhandling="pass", .init=c(), .combine=c) %dopar% {
+#           cat(paste0("\nFitting ", model, ":", country, ":", factor))
+#           genResampleData(modelType=model, 
+#                           countryAbbrev=country,  
+#                           factor=factor,
+#                           n=opts$resamples, 
+#                           method=opts$method, 
+#                           clobber=opts$clobber,
+#                           baseHistorical=opts$baseHistorical,
+#                           baseResample=opts$baseResample)
+#         }
+#       }
+#     } else if ((model == "cd") || (model == "ces")) {
+#       # There are no energy types or factors over which to loop.
+#       foreach(country=opts$country, .errorhandling="pass", .init=c(), .combine=c) %dopar% {
+#         cat(paste0("\nFitting ", model, ":", country))
+#         genResampleData(modelType=model, 
+#                         countryAbbrev=country,  
+#                         n=opts$resamples, 
+#                         method=opts$method, 
+#                         clobber=opts$clobber,
+#                         baseHistorical=opts$baseHistorical,
+#                         baseResample=opts$baseResample)
+#       }
+#     } else {
+#       # We get here if we have "cde", "cese-(xy)z", or linex models. 
+#       # Need to account for energy types.
+#       # But, only do those countries for which the requested energy type is available.
+#       for (energy in opts$energy){
+#         if (energy == "U"){
+#           availableCountries <- intersect(opts$country, countryAbbrevsU)
+#         } else {
+#           availableCountries <- intersect(opts$country, countryAbbrevs)
+#         }
+#         foreach(country=availableCountries, .errorhandling="pass", .init=c(), .combine=c) %dopar% {
+#           cat(paste0("\nFitting ", model, ":", country, ":", energy))
+#           genResampleData(modelType=model, 
+#                           countryAbbrev=country,  
+#                           energyType=energy, 
+#                           n=opts$resamples, 
+#                           method=opts$method, 
+#                           clobber=opts$clobber,
+#                           baseHistorical=opts$baseHistorical,
+#                           baseResample=opts$baseResample)
+#         }
+#       }
+#     }
+#   }
+}
+
+cat("\n\nDone @ ")
+cat(date())
+cat('\n\n')
+cat("duration:\n")
+print(proc.time() - startTime)
+cat('\n\n')
