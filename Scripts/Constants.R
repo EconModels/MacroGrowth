504ac1e46da1e58cb5e55c4c20ed0512f5fc1e12
MatthewHeun <matthew.heun@me.com>
2014-04-24 16:17:28 -0400

    Reorganized the constants file.

--- Scripts/Constants.R
+++ Scripts/Constants.R
@@ -7,8 +7,19 @@ countryNamesAlph <- c(CN="China", IR="Iran", JP="Japan",
                       SA="Saudi Arabia", TZ="Tanzania", UK="United Kingdom", 
                       US="USA", ZA="South Africa", ZM="Zambia") 
 countryNamesAlphU <- c(JP="Japan", UK="United Kingdom", US="USA") 
+# Models and nests
 modelTypes <- c('sf', 'cd', 'cde', 'ces', 'cese-(kl)e', 'cese-(le)k', 'cese-(ek)l', 'linex')
 cesNests <- c(kl="(kl)", kle="(kl)e", lek="(le)k", ekl="(ek)l")
+# Resampling information
 resampleMethods <- c("resample", "residual", "wild", "debug")
-energyTypes <- c(Q="iQ", X="iX", U="iU") 
-factors <- c(K="iK", L="iL", energyTypes)
+# logical names for variables. These are the names of the columns in our data set
+Timevar <- "iYear"
+GDPvar <- "iY"
+Kvar <- "iK"
+Lvar <- "iL"
+Qvar <- "iQ"
+Xvar <- "iX"
+Uvar <- "iU"
+# Energy types and factors
+energyTypes <- c(Q=Qvar, X=Xvar, U=Uvar) 
+factors <- c(K=Kvar, L=Lvar, energyTypes)
