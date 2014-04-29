69a5b4b79670e5aab4e5186aac2777d47133dce2
MatthewHeun <matthew.heun@me.com>
2014-04-28 21:57:38 -0400

    Improved documentation for loadResampledData.

--- Packages/EconData/R/loaders.R
+++ Packages/EconData/R/loaders.R
@@ -3,12 +3,21 @@ is.in <- function( el, set ) {
   is.element(el, set)
 }
 
+#' Loads resampled coefficients from files
+#' 
+#' @param path the directory containing the resampled coefficient files
+#' @param country a country or vector of countries.
+#' @param model a model or vector of models.
+#' @param factors strings of factors used in fitting the models.
+#' @param sep the separator in the file names between country, model, and factors
+#' @return a string representing the energyType that was used for the fitting. NA if the sfModel was used.
+#' @export
 loadResampledData <- function( path, country=NULL, model=NULL, factors=NULL, sep="_" ) {
   files <- dir(path)
-  # Remove any files that begin with "models_". These are model files, not coefficient files.
+  # Remove any files that contains the string "models_". These are model files, not coefficient files.
   toRemove <- grepl(pattern="models_", x=files, fixed=TRUE)
   files <- files[!toRemove]
-  # Remove the .Rdata suffix from the files.
+  # Remove the .Rdata suffix from the file names.
   names <- gsub(pattern=".Rdata", replacement="", x=files, fixed=TRUE)
   pieces <- strsplit( x=names, split=sep )
   keep <- sapply( pieces, 
@@ -16,9 +25,6 @@ loadResampledData <- function( path, country=NULL, model=NULL, factors=NULL, sep
   files <- files[ keep ]
   # the next line could be augmented to add additional information to the 
   # data frame.
-  dflist <- lapply(files, function(x) { 
-                              readRDS ( file.path(path, x) ) 
-                          }
-                   )
+  dflist <- lapply(files, function(x) { readRDS ( file.path(path, x) ) } )
   do.call( rbind.fill, dflist )
 }
