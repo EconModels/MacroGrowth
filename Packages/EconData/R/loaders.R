bac7473de0216c03edfa88ccc2e25ae7679be647
MatthewHeun <matthew.heun@me.com>
2014-04-29 12:54:12 -0400

    loadResampledData now works with an archive.

--- Packages/EconData/R/loaders.R
+++ Packages/EconData/R/loaders.R
@@ -5,26 +5,44 @@ is.in <- function( el, set ) {
 
 #' Loads resampled coefficients from files
 #' 
-#' @param path the directory containing the resampled coefficient files
+#' @param path the directory containing the resampled coefficient files. Do not add a trailing file separator.
+#' @param archive a zip archive containing resampled coefficient files
 #' @param country a country or vector of countries.
 #' @param model a model or vector of models.
 #' @param factors strings of factors used in fitting the models.
 #' @param sep the separator in the file names between country, model, and factors
 #' @return a string representing the energyType that was used for the fitting. NA if the sfModel was used.
 #' @export
-loadResampledData <- function( path, country=NULL, model=NULL, factors=NULL, sep="_" ) {
-  files <- dir(path)
+loadResampledData <- function( path, archive=NULL, country=NULL, model=NULL, factors=NULL, sep="_" ) {
+  if (is.null(archive)){
+    files <- dir(path)
+  } else {
+    filesDF <- unzip(zipfile=archive, list=TRUE)
+    files <- filesDF[["Name"]]
+    # Keep only those files with the ".Rdata" extension
+    files <- files[grepl(pattern=".Rdata", x=files, fixed=TRUE)]
+  }
   # Remove any files that contains the string "models_". These are model files, not coefficient files.
   toRemove <- grepl(pattern="models_", x=files, fixed=TRUE)
   files <- files[!toRemove]
   # Remove the .Rdata suffix from the file names.
   names <- gsub(pattern=".Rdata", replacement="", x=files, fixed=TRUE)
+  # Remove the path prefix from the file names, if present
+  names <- gsub(pattern=path, replacement="", x=names, fixed=TRUE)
+  # Remove any file separators, if present.
+  names <- gsub(pattern=.Platform$file.sep, replacement="", x=names, fixed=TRUE)
   pieces <- strsplit( x=names, split=sep )
   keep <- sapply( pieces, 
                   function(x) { is.in(x[1],country) && is.in(x[2],model) &&  is.in(x[3],factors) } )
   files <- files[ keep ]
   # the next line could be augmented to add additional information to the 
   # data frame.
-  dflist <- lapply(files, function(x) { readRDS ( file.path(path, x) ) } )
+  if (is.null(archive)){
+    # Read files from the directory on disk
+    dflist <- lapply(files, function(x) { readRDS ( file.path(path, x) ) } )
+  } else {
+    # Read files from the archive
+    dflist <- lapply(files, function(x) { readRDS(gzcon(unz(archive, x))) } )
+  }
   do.call( rbind.fill, dflist )
 }
