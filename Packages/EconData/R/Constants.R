8ceb3dcc169d0022ae89ceaf7913f44b2b75a992
MatthewHeun <matthew.heun@me.com>
2014-05-18 18:21:15 +0100

    Commented ununsed constants. Remove later if this doesn't cause any problems.

--- Packages/EconData/R/Constants.R
+++ Packages/EconData/R/Constants.R
@@ -1,20 +1,37 @@
 # List of countries in our data set.
 #' @export
 countryAbbrevs <- c(US="US", UK="UK", JP="JP", CN="CN", ZA="ZA", SA="SA", IR="IR", TZ="TZ", ZM="ZM")
-countryAbbrevsAlph <- sort(countryAbbrevs)
-countryAbbrevsU <- c(US="US", UK="UK", JP="JP") # Only these countries have useful work data
-countryAbbrevsAlphU <- sort(countryAbbrevsU)
-countryNamesAlph <- c(CN="China", IR="Iran", JP="Japan", 
-                      SA="Saudi Arabia", TZ="Tanzania", UK="United Kingdom", 
-                      US="USA", ZA="South Africa", ZM="Zambia") 
-countryNamesAlphU <- c(JP="Japan", UK="United Kingdom", US="USA") 
+# countryAbbrevsAlph <- sort(countryAbbrevs)
+# countryAbbrevsU <- c(US="US", UK="UK", JP="JP") # Only these countries have useful work data
+# countryAbbrevsAlphU <- sort(countryAbbrevsU)
+# countryNamesAlph <- c(CN="China", IR="Iran", JP="Japan", 
+#                       SA="Saudi Arabia", TZ="Tanzania", UK="United Kingdom", 
+#                       US="USA", ZA="South Africa", ZM="Zambia") 
+# countryNamesAlphU <- c(JP="Japan", UK="United Kingdom", US="USA") 
 # Models and nests
-modelTypes <- c('sf', 'cd', 'cde', 'ces', 'cese-(kl)e', 'cese-(le)k', 'cese-(ek)l', 'linex')
-cesNests <- c(kl="(kl)", kle="(kl)e", lek="(le)k", ekl="(ek)l")
+# modelTypes <- c('sf', 'cd', 'cde', 'ces', 'cese-(kl)e', 'cese-(le)k', 'cese-(ek)l', 'linex')
+# cesNests <- c(kl="(kl)", kle="(kl)e", lek="(le)k", ekl="(ek)l")
 # Resampling information
-resampleMethods <- c("resample", "residual", "wild", "debug")
+# resampleMethods <- c("resample", "residual", "wild", "debug")
 # Energy types and factors
 #' @export
 energyTypes <- c(Q="iQ", X="iX", U="iU") 
 #' @export
+energyLevels <- c(none="none", energyTypes)
+#' @export
 factors <- c(K="iK", L="iL", energyTypes)
+
+# Preferred order for nestStr
+#' @export
+nestStrLevels <- c("iK", "iL", "iQ", "iX", "iU", 
+                   "iK+iL", 
+                   "iK+iL+iQ", "iK+iL+iX", "iK+iL+iU",
+                   "iL+iQ+iK", "iL+iX+iK", "iL+iU+iK",
+                   "iQ+iK+iL", "iX+iK+iL", "iU+iK+iL")
+
+# Preferred order for nest
+#' @export
+nestLevels <- c("(iK + iL) + ()", 
+                "(iK + iL) + (iQ)", "(iK + iL) + (iX)", "(iK + iL) + (iU)",
+                "(iL + iQ) + (iK)", "(iL + iX) + (iK)", "(iL + iU) + (iK)",
+                "(iQ + iK) + (iL)", "(iX + iK) + (iL)", "(iU + iK) + (iL)")
