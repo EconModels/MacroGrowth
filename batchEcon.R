1cf4c0f10bcec296da5d32bd5bc767e2af46dda2
MatthewHeun <matthew.heun@me.com>
2014-06-09 14:15:54 -0400

    Added more example usage.

--- batchEcon.R
+++ batchEcon.R
@@ -4,11 +4,17 @@
 
 # Move into the top level directory that contains this file
 # for US, thermal energy, single factor model, 10 resamples, clobbering previous results, and wild resampling:
-# ./batchEcon.R -c US -f iQ -m sf -n 10 -C -M wild -H data -R data_resample
+# ./batchEcon.R -c US -f iQ -m sf -n 10 -C -M wild -R data_resample
+
 # for US, exergy, linex model, 10 resamples, clobbering previous results, and wild resampling:
-# ./batchEcon.R -c US -e iX -m linex -n 10 -C -M wild -H data -R data_resample
+# ./batchEcon.R -c US -e iX -m linex -n 10 -C -M wild -R data_resample
+
+# for US, all energy types, fast models (everything except CES with energy), 
+#     10 resamples, clobber previous results, wild resampling:
+# ./batchEcon.R -c US -e all -f all -m fast -n 2 -C -M wild -R data_resample
+
 # for all countries, all energy types, all models, 1000 resamples, clobber previous results, wild resampling:
-# ./batchEcon.R -c all -e all -m all -n 1000 -C -M wild -H data -R data_resample
+# ./batchEcon.R -c all -e all -m all -n 1000 -C -M wild -R data_resample
 
 require(EconModels)
 require(EconData)
