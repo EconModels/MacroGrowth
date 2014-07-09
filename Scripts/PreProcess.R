8d87363f2681aa0c03f66b150a9955ba4c70415e
MatthewHeun <matthew.heun@me.com>
2014-07-09 14:51:53 -0400

    Put data loading in a loop. Makes it easier to add future data sets.

--- Scripts/PreProcess.R
+++ Scripts/PreProcess.R
@@ -22,21 +22,21 @@ cat('\n')
 datadir <- file.path("Packages", "EconData", "data")
 
 #
-# Load historical data and save the data frame.
+# Load historical data sets and save the data frames.
 #
 cat("Loading and saving historical data..."); cat("\n")
-# Econ2011 Data Set
-cat("  Econ2011..."); cat("\n")
-Econ2011 <- read.table(file.path("data", "Econ2011.txt"), header=TRUE)
-Econ2011CountryLevels <- countryAbbrevs[countryAbbrevs %in% levels(Econ2011$Country)]
-for (lev in rev(Econ2011CountryLevels)) { Econ2011$Country <- relevel(Econ2011$Country, ref=lev) }
-save(Econ2011, file=file.path(datadir, "Econ2011.rda"), compress="gzip")
-# Warr2000 Data Set
-cat("  Warr2000..."); cat("\n")
-Warr2000 <- read.table(file.path("data", "Warr2000.txt"), header=TRUE)
-Warr2000CountryLevels <- countryAbbrevs[countryAbbrevs %in% levels(Warr2000$Country)]
-for (lev in rev(Warr2000CountryLevels)) { Warr2000$Country <- relevel(Warr2000$Country, ref=lev) }
-save(Warr2000, file=file.path(datadir, "Warr2000.rda"), compress="gzip")
+databases <- c("Econ2011", "Warr2000")
+for (file in databases){
+  cat("  "); cat(file); cat("\n")
+  infile <- paste0(file, ".txt")
+  outfile <- paste0(file, ".rda")
+  data <- read.table(file.path("data", infile), header=TRUE)
+  # Get the countries that are in this database, in the order we'd like the levels
+  CountryLevels <- countryAbbrevs[countryAbbrevs %in% levels(data$Country)]
+  # Releve the country column
+  for (lev in rev(CountryLevels)) { data$Country <- relevel(data$Country, ref=lev) }
+  save(data, file=file.path(datadir, outfile), compress="gzip")
+}
 
 #
 # Rebuild the EconData package
