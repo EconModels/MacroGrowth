e5f721aebfc2cb23965ecfd200d80e600a9d08b6
MatthewHeun <matthew.heun@me.com>
2014-07-10 10:36:51 -0400

    Deleted unused lines in Constants.R.

--- Packages/EconData/R/Constants.R
+++ Packages/EconData/R/Constants.R
@@ -1,18 +1,6 @@
 # List of countries in our data set.
 #' @export
 countryAbbrevs <- c(US="US", UK="UK", JP="JP", AT="AT", CN="CN", ZA="ZA", SA="SA", IR="IR", TZ="TZ", ZM="ZM")
-# countryAbbrevsAlph <- sort(countryAbbrevs)
-# countryAbbrevsU <- c(US="US", UK="UK", JP="JP") # Only these countries have useful work data
-# countryAbbrevsAlphU <- sort(countryAbbrevsU)
-# countryNamesAlph <- c(CN="China", IR="Iran", JP="Japan", 
-#                       SA="Saudi Arabia", TZ="Tanzania", UK="United Kingdom", 
-#                       US="USA", ZA="South Africa", ZM="Zambia") 
-# countryNamesAlphU <- c(JP="Japan", UK="United Kingdom", US="USA") 
-# Models and nests
-# modelTypes <- c('sf', 'cd', 'cde', 'ces', 'cese-(kl)e', 'cese-(le)k', 'cese-(ek)l', 'linex')
-# cesNests <- c(kl="(kl)", kle="(kl)e", lek="(le)k", ekl="(ek)l")
-# Resampling information
-# resampleMethods <- c("resample", "residual", "wild", "debug")
 # Energy types and factors
 #' @export
 energyTypes <- c(Q="iQ", X="iX", U="iU") 
