a4ebed7e2c5b0e29f95fec2d65dfe8f7e37b20f0
MatthewHeun <matthew.heun@me.com>
2014-07-23 21:53:13 -0400

    Now using inputfile variable.

--- Scripts/PreProcess.R
+++ Scripts/PreProcess.R
@@ -25,7 +25,8 @@ outputdir <- file.path("Packages", "EconData", "data")
 #
 for (src in dataSources){
   cat(paste("Loading and saving", src, "historical data ...")); cat("\n")
-  data <- read.table(file.path("data", paste0(src, ".txt")), header=TRUE)
+  inputfile <- file.path("data", paste0(src, ".txt"))
+  data <- read.table(inputfile, header=TRUE)
   data$Country <- relevelFactor(data$Country, countryAbbrevs)
   assign(src, data)
   save(list=src, file=file.path(outputdir, paste0(src, ".rda")))
