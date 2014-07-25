6547f104c59e94049b0b8a3ffcaf702c1ebe9ffe
MatthewHeun <matthew.heun@me.com>
2014-07-24 20:06:09 -0400

    PostProcess.R now accommodates multiple Sources.

--- Scripts/PostProcess.R
+++ Scripts/PostProcess.R
@@ -2,16 +2,15 @@
 #
 # This script creates all objects and moves them into place for
 # the EconData package.
-# Execute this script from the directory that contains "data_resample."
-# You'll probably use the command "Scripts/PostProcess.R"
+# You'll probably use the command "Scripts/PostProcess.R -R data_resample/<Source>" 
+# where <Source> is the name of a data source, such as "Calvin" or "REXS".
 #
 # Run this script after you have done all analyses, 
-# possibly by executing the "batch.bash" script.
-# This script assumes that models and data
-# live in the directory called "data_resample." ("batch.bash" does this by default.)
+# probably by executing a <Source>_batch.bash script.
 
 require(EconData)
 require(EconModels)
+require(optparse)
 require(doParallel)
 registerDoParallel()
 
@@ -20,58 +19,79 @@ cat("\n\nStart @ ")
 cat(date())
 cat('\n')
 
-# Directory into which objects should be saved
-datadir <- file.path("Packages", "EconData", "data")
+# Provide a way to specify directory of resampled data
+option_list <- list(
+  make_option(c("-R", "--resamplePath"), default="data_resample/Calvin",
+              help="path to resampled data [default=%default]")
+)
+# Parse the option list
+opts <- parse_args(OptionParser(option_list=option_list))
+print(opts)
+
+# Get the data Source from the resamplePath.
+dir_pieces <- strsplit(opts$resamplePath, split=.Platform$file.sep)[[1]]
+Source <- dir_pieces[length(dir_pieces)]
+
+# Directory into which objects should be saved for the EconData pacakge
+outputdir <- file.path("Packages", "EconData", "data")
 
 #
 # Load all coefficients. Do this task in parallel for a (minor) speed gain.
 #
-cat("Loading and saving all coefficients..."); cat("\n")
+cat(paste("Loading and saving", Source, "coefficients...")); cat("\n")
 # .errorhandling="remove" skips missing countries.
-AllCoef <- foreach(country=countryAbbrevs, .combine=rbind, .errorhandling="remove") %dopar% {
-  loadResampledData(path="data_resample", country=country, kind="coeffs")
+Coeffs <- foreach(country=countryAbbrevs, .combine=rbind, .errorhandling="remove") %dopar% {
+  loadResampledData(path=opts$resamplePath, country=country, kind="coeffs")
 }
 # This next code can be used to ensure that the results are identical. 
 # But, you should change countryAbbrevs to sort(countryAbbrevs) above
-# AllCoef2 <- loadResampledData(path="data_resample", kind="coeffs")
-# print(identical(AllCoef, AllCoef2))
+# coeffs2 <- loadResampledData(path=opts$resamplePath, kind="coeffs")
+# print(identical(coeffs, coeffs2))
 
-# Relevel the country abbreviations, nestStr, nestStrParen, and energy in AllCoef
-AllCoef$country <- relevelFactor(as.factor(AllCoef$country), countryAbbrevs)
-AllCoef$nestStr <- relevelFactor(as.factor(AllCoef$nestStr), nestStrLevels)
-AllCoef$nestStrParen <- relevelFactor(as.factor(AllCoef$nestStrParen), nestStrParenLevels)
-AllCoef$energy <- replace(AllCoef$energy, which(is.na(AllCoef$energy)), "none")
-AllCoef$energy <- relevelFactor(as.factor(AllCoef$energy), energyLevels)
+# Relevel the country abbreviations, nestStr, nestStrParen, and energy in coeffs
+Coeffs$country <- relevelFactor(as.factor(Coeffs$country), levs=countryAbbrevs)
+Coeffs$nestStr <- relevelFactor(as.factor(Coeffs$nestStr), levs=nestStrLevels)
+Coeffs$nestStrParen <- relevelFactor(as.factor(Coeffs$nestStrParen), levs=nestStrParenLevels)
+Coeffs$energy <- replace(Coeffs$energy, which(is.na(Coeffs$energy)), "none")
+Coeffs$energy <- relevelFactor(as.factor(Coeffs$energy), levs=energyLevels)
 
 # Save all coefficients in one data frame
-save(AllCoef, file=file.path(datadir, "AllCoef.rda"), compress="gzip")
+objectname <- paste0(Source, "_Coeffs")
+outpath <- file.path(outputdir, paste0(objectname, ".rda"))
+assign(objectname, Coeffs)
+save(list=objectname, file=outpath, compress="gzip")
 
 #
 # Load all fitted models
 #
-cat("Loading and saving all fitted models..."); cat("\n")
+cat(paste("Loading and saving", Source, "fitted models...")); cat("\n")
 # .errorhandling="remove" skips missing countries.
-AllFitted <- foreach(country=countryAbbrevs, .combine=rbind, .errorhandling="remove") %dopar% {
-  loadResampledData(path="data_resample", country=country, kind="fitted")
+Fitted <- foreach(country=countryAbbrevs, .combine=rbind, .errorhandling="remove") %dopar% {
+  loadResampledData(path=opts$resamplePath, country=country, kind="fitted")
 }
-# system.time(AllFitted2 <- loadResampledData(path="data_resample", kind="fitted"))
-# Relevel the country abbreviations, nestStr, nestStrParen, and energy in AllFitted
-AllFitted$Country <- relevelFactor(as.factor(AllFitted$Country), countryAbbrevs)
-AllFitted$nestStr <- relevelFactor(as.factor(AllFitted$nestStr), nestStrLevels)
-AllFitted$nestStrParen <- relevelFactor(as.factor(AllFitted$nestStrParen), nestStrParenLevels)
-AllFitted$energy <- replace(AllFitted$energy, which(is.na(AllFitted$energy)), "none")
-AllFitted$energy <- relevelFactor(as.factor(AllFitted$energy), energyLevels)
+# system.time(Fitted2 <- loadResampledData(path="data_resample", kind="fitted"))
+# Relevel the country abbreviations, nestStr, nestStrParen, and energy in Fitted
+Fitted$Country <- relevelFactor(as.factor(Fitted$Country), levs=countryAbbrevs)
+Fitted$nestStr <- relevelFactor(as.factor(Fitted$nestStr), levs=nestStrLevels)
+Fitted$nestStrParen <- relevelFactor(as.factor(Fitted$nestStrParen), levs=nestStrParenLevels)
+Fitted$energy <- replace(Fitted$energy, which(is.na(Fitted$energy)), "none")
+Fitted$energy <- relevelFactor(as.factor(Fitted$energy), levs=energyLevels)
 
 # Save all fitted models in one data frame
-save(AllFitted, file=file.path(datadir, "AllFitted.rda"), compress="gzip")
+objectname <- paste0(Source, "_Fitted")
+outpath <- file.path(outputdir, paste0(objectname, ".rda"))
+assign(objectname, Fitted)
+save(list=objectname, file=outpath, compress="gzip")
 
 #
 # Create an archive of the results
 #
-cat("Creating archive..."); cat("\n")
-zip(zipfile="data_resample.zip", files="data_resample", flags="-r9Xj")
+cat(paste0("Creating archive for ", Source, "...")); cat("\n")
+zip(zipfile="data_resample.zip", files="data_resample", flags="-r9X")
 
+#
 # Build EconData package here?
+#
 
 cat("\n\nDone @ ")
 cat(date())
