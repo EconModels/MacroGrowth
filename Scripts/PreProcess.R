867799a8bf6452de5e9234b99885600653da9b0a
MatthewHeun <matthew.heun@me.com>
2014-07-29 14:50:22 -0400

    ProProcess.R now has a -S argument.

--- Scripts/PreProcess.R
+++ Scripts/PreProcess.R
@@ -1,21 +1,28 @@
 #!/usr/bin/Rscript
 #
-# This script creates all objects and moves them into place for
+# This script creates a data frame for the specified <Source> and moves it into place for
 # the EconData package.
-# Execute this script from the directory that contains "data_resample."
-# You'll probably use the command "Scripts/PostProcess.R"
+# Execute this script from top level of the repository (Econ-Growth-R-Analysis).
+# You'll probably use the command "Scripts/PreProcess.R"
 #
-# Run this script after you have done all analyses, 
-# possibly by executing the "batch.bash" script.
-# This script assumes that models and data
-# live in the directory called "data_resample." ("batch.bash" does this by default.)
+# Run this script before you do resampling analyses.
 
 require(EconData)
+require(optparse)
 
 startTime <- proc.time()
 cat("\n\nStart @ ")
 cat(date())
 cat('\n')
+
+# Provide a way to specify data source
+option_list <- list(
+  make_option(c("-S", "--Source"), default="Calvin",
+              help="Source of data [default=%default]")
+)
+
+opts <- parse_args(OptionParser(option_list=option_list))
+
 
 # Directory into which objects should be saved
 outputdir <- file.path("Packages", "EconData", "data")
@@ -23,14 +30,12 @@ outputdir <- file.path("Packages", "EconData", "data")
 #
 # Load historical data sets and save the data frames for inclusion in the EconData package.
 #
-for (src in dataSources){
-  cat(paste("Loading and saving", src, "historical data ...")); cat("\n")
-  inputpath <- file.path("data", paste0(src, ".txt"))
-  data <- read.table(inputpath, header=TRUE)
-  data$Country <- relevelFactor(data$Country, levs=countryAbbrevs)
-  assign(src, data)
-  save(list=src, file=file.path(outputdir, paste0(src, ".rda")))
-}
+cat(paste("Loading and saving", opts$Source, "historical data ...")); cat("\n")
+inputpath <- file.path("data", paste0(opts$Source, ".txt"))
+data <- read.table(inputpath, header=TRUE)
+data$Country <- relevelFactor(data$Country, levs=countryAbbrevs)
+assign(opts$Source, data)
+save(list=opts$Source, file=file.path(outputdir, paste0(opts$Source, ".rda")))
 
 #
 # Rebuild the EconData package
