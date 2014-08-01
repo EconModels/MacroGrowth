1ed961dc6fe736350ef0e9ad14427d255f87ad1c
MatthewHeun <matthew.heun@me.com>
2014-07-31 21:47:28 -0400

    Updated Constants.R file. Accounts for more energy types and nests.

--- Packages/EconData/R/Constants.R
+++ Packages/EconData/R/Constants.R
@@ -3,7 +3,8 @@
 countryAbbrevs <- c(US="US", UK="UK", JP="JP", AT="AT", CN="CN", ZA="ZA", SA="SA", IR="IR", TZ="TZ", ZM="ZM")
 # Energy types and factors
 #' @export
-energyTypes <- c(Q="iQ", X="iX", U="iU") 
+# energyTypes <- c(Q="iQ", X="iX", U="iU") 
+energyTypes <- c(Qp="iQp", Xp="iXp", Qf="iQf", Xf="iXf, U="iU") 
 #' @export
 energyLevels <- c(none="none", energyTypes)
 #' @export
@@ -11,19 +12,38 @@ factors <- c(K="iK", L="iL", energyTypes)
 
 # Preferred order for nestStr
 #' @export
-nestStrLevels <- c("iK", "iL", "iQ", "iX", "iU", 
+# nestStrLevels <- c("iK", "iL", "iQ", "iX", "iU", 
+#                    "iK+iL", 
+#                    "iK+iL+iQ", "iK+iL+iX", "iK+iL+iU",
+#                    "iL+iQ+iK", "iL+iX+iK", "iL+iU+iK",
+#                    "iQ+iK+iL", "iX+iK+iL", "iU+iK+iL")
+nestStrLevels <- c("iK", "iL", "iQp", "iXp", "iQf", "iXf", "iU", 
                    "iK+iL", 
-                   "iK+iL+iQ", "iK+iL+iX", "iK+iL+iU",
-                   "iL+iQ+iK", "iL+iX+iK", "iL+iU+iK",
-                   "iQ+iK+iL", "iX+iK+iL", "iU+iK+iL")
+                   "iK+iL+iQp", "iK+iL+iXp", "iK+iL+iQf", "iK+iL+iXf", "iK+iL+iU",
+                   "iL+iQp+iK", "iL+iXp+iK", "iL+iQf+iK", "iL+iXF+iK", "iL+iU+iK",
+                   "iQp+iK+iL", "iXp+iK+iL", "iQf+iK+iL", "iXf+iK+iL", "iU+iK+iL")
 
 # Preferred order for nestStrParen
 #' @export
-nestStrParenLevels <- c("(iK) + ()", "(iL) + ()", "(iQ) + ()", "(iX) + ()" , "(iU) + ()",
+# nestStrParenLevels <- c("(iK) + ()", "(iL) + ()", "(iQ) + ()", "(iX) + ()" , "(iU) + ()",
+#                         "(iK + iL) + ()", 
+#                         "(iK + iL) + (iQ)", "(iK + iL) + (iX)", "(iK + iL) + (iU)",
+#                         "(iL + iQ) + (iK)", "(iL + iX) + (iK)", "(iL + iU) + (iK)",
+#                         "(iQ + iK) + (iL)", "(iX + iK) + (iL)", "(iU + iK) + (iL)")
+nestStrParenLevels <- c("(iK) + ()", 
+                        "(iL) + ()", 
+                        "(iQp) + ()", 
+                        "(iXp) + ()", 
+                        "(iQf) + ()", 
+                        "(iXf) + ()", 
+                        "(iU) + ()",
                         "(iK + iL) + ()", 
-                        "(iK + iL) + (iQ)", "(iK + iL) + (iX)", "(iK + iL) + (iU)",
-                        "(iL + iQ) + (iK)", "(iL + iX) + (iK)", "(iL + iU) + (iK)",
-                        "(iQ + iK) + (iL)", "(iX + iK) + (iL)", "(iU + iK) + (iL)")
+                        "(iK + iL) + (iQp)", "(iK + iL) + (iXp)", 
+                        "(iK + iL) + (iQf)", "(iK + iL) + (iXf)", "(iK + iL) + (iU)",
+                        "(iL + iQp) + (iK)", "(iL + iXp) + (iK)", 
+                        "(iL + iQf) + (iK)", "(iL + iXf) + (iK)", "(iL + iU) + (iK)",
+                        "(iQp + iK) + (iL)", "(iXp + iK) + (iL)", 
+                        "(iQf + iK) + (iL)", "(iXf + iK) + (iL)", "(iU + iK) + (iL)")
 
 #' Relevels a factor based on given levels
 #' 
