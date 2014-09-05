08e8a2fa0352f1cb4bd7f20cd31f9f0da8b65a30
Randall Pruim <rpruim@gmail.com>
2014-09-05 01:00:46 -0400

    stub for linexModel() checks; check cdModel() with K and L reversed.

--- Packages/EconModels/inst/tests/test-model-fits.R
+++ Packages/EconModels/inst/tests/test-model-fits.R
@@ -15,13 +15,24 @@ test_that("cdModel() fits are correct", {
   # Fit
   modelFree <- cdModel(fitGDP ~ iK + iL + iYear, data = testData, constrained = FALSE, 
                        save.data = TRUE)
-  
   expect_equivalent(naturalCoef(modelFree)[, c("alpha","beta","gamma","lambda"), drop=TRUE], 
                     list(alpha, beta, 0, lambda) )
   
+  modelFree2 <- cdModel(fitGDP ~ iL + iK + iYear, data = testData, constrained = FALSE, 
+                       save.data = TRUE)
+  expect_equivalent(naturalCoef(modelFree2)[, c("alpha","beta","gamma","lambda"), drop=TRUE], 
+                    list(beta, alpha, 0, lambda) )
+  
   modelConstrained <- cdModel(fitGDP ~ iK + iL + iYear, data = testData, 
                               constrained = TRUE, save.data = TRUE)
-  
   expect_equivalent(naturalCoef(modelConstrained)[, c("alpha", "beta", "gamma"), drop=TRUE], 
                     list(0, 1, 0))
+})
+
+test_that("linexModel() fits are correct", {
+  
+  # add tests here.
+  expect_equal(1,1)
+  
+  
 })
