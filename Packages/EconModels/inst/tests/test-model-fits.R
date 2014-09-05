b33e1e5f423525fd25b5adc522042da2b080b7db
Randall Pruim <rpruim@gmail.com>
2014-09-05 00:02:24 -0400

    newer roxygen

--- Packages/EconModels/inst/tests/test-model-fits.R
+++ Packages/EconModels/inst/tests/test-model-fits.R
@@ -4,24 +4,22 @@ context('Testing Model Fits')
 test_that("cdModel() fits are correct", {
 
   # Use the US factors of production from the Calvin source
-  testData <- subset(Calvin, Country=="US")  
+  testData <- subset(EconData::Calvin, Country=="US")  
   
   # Cobb-Douglas without energy
   alpha <- -0.1
   beta <- 1.1
   lambda <- 0.02
-  testData$fitGDP <- exp(lambda * cData$iYear) * cData$iK^alpha * cData$iL^beta
+  testData$fitGDP <- exp(lambda * testData$iYear) * testData$iK^alpha * testData$iL^beta
   
   # Fit
-  modelFree <- cdModel(fitGDP ~ iK + iL + iYear, data = cData, constrained = FALSE, 
+  modelFree <- cdModel(fitGDP ~ iK + iL + iYear, data = testData, constrained = FALSE, 
                        save.data = TRUE)
   
-  expect_equal(modelFree$coefficients[["logscale"]], 0.0 )
-  expect_equal(modelFree$coefficients[["lambda"]], lambda)
-  expect_equal(modelFree$coefficients[["alpha"]], alpha )
+  expect_equivalent(coef(modelFree), c(0.0, alpha, lambda) )
   
-  modelConstrained <- cdModel(fitGDP ~ iK + iL + iYear, data = cData, 
+  modelConstrained <- cdModel(fitGDP ~ iK + iL + iYear, data = testData, 
                               constrained = TRUE, save.data = TRUE)
   
-  expect_equal(modelContrained$coefficients[["alpha"]], 0.0)
+  expect_equal(coef(modelConstrained)["alpha"], 0.0)
 })
