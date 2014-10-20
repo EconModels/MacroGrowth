59c695237a1d1cd532dff86572ad16cbb2c7feb3
Randall Pruim <rpruim@gmail.com>
2014-10-20 12:41:38 -0400

    better name handling

--- Packages/EconModels/R/leaf_apply.R
+++ Packages/EconModels/R/leaf_apply.R
@@ -2,19 +2,27 @@
 leaf_apply <- function ( l, f, class=NULL, name="", sep="." ) {
   res <- list()
   list_names <- names(l)
+  if (is.null(list_names)) {
+    list_names <- as.character(1L: length(l)) 
+  }
   for (idx in 1:length(l)) {
     item <- l[[idx]]
-    item_name <- paste0(name, list_names[idx], sep=sep)
+    if (nchar(name) > 0) {
+      item_name <- paste(name, list_names[idx], sep=sep)
+    } else {
+      item_name <- list_names[idx]
+    }
+    
     if (is.null(class)) {
       if (is.list(item)) {
         res <- c(res, leaf_apply(item, f, class=NULL, name=item_name))
       } else {
-        res <- c(res, list(f(item)))
+        res <- c(res, list(f(item, item_name)))
         names(res)[length(res)] <- item_name
       }
     } else {
       if ( length(intersect(class(item) , class)) > 0L ) {
-        res <- c(res, list(f(item)))
+        res <- c(res, list(f(item, item_name)))
         names(res)[length(res)] <- item_name
       } else {
         if (is.list(item)) { res <- c(res, leaf_apply(item, f, class=class, name=item_name)) }
@@ -22,4 +30,8 @@ leaf_apply <- function ( l, f, class=NULL, name="", sep="." ) {
     }
   }
   res
+}
+
+extract_resid <- function(x, name) {
+  data.frame(resid = resid(x), name=name)
 }
