231e714ec0951a9093e4b64ff669f214e4544628
Randall Pruim <rpruim@gmail.com>
2015-05-14 14:17:32 +1200

    use deparse(formula) in place of as.character(formula)  comment out some more cruft

--- Packages/EconModels/R/formulaUtils.R
+++ Packages/EconModels/R/formulaUtils.R
@@ -25,7 +25,7 @@ build_formula <- function( left, right, env=parent.frame() ) {
 # this isn't very robust if "terms" have + in them.  But for simple things it will work.
 
 keep_rhs <- function(formula, n, env=parent.frame()) {
-  formulaString <- as.character(formula)
+  formulaString <- deparse(formula)
   formulaSplit <- strsplit(formulaString, " ~ ")[[1]]
   l <- formulaSplit[1]
   r <- strsplit( formulaSplit[2], " \\+ ")[[1]]
@@ -33,14 +33,14 @@ keep_rhs <- function(formula, n, env=parent.frame()) {
 }
 
 extract_rhs <- function(formula, n) {
-  formulaString <- as.character(formula)
+  formulaString <- deparse(formula)
   formulaSplit <- strsplit(formulaString, " ~ ")[[1]]
   strsplit( formulaSplit[2], " \\+ ")[[1]][n]
   }
 
 
 replace_rhs <- function(formula, n, term, env=parent.frame()) {
-  formulaString <- as.character(formula)
+  formulaString <- deparse(formula)
   formulaSplit <- strsplit(formulaString, " ~ ")[[1]]
   l <- formulaSplit[1]
   r <- strsplit( formulaSplit[2], " \\+ ")[[1]]
@@ -48,8 +48,7 @@ replace_rhs <- function(formula, n, term, env=parent.frame()) {
   build_formula( l, r, env=env)
 }
 
-# # examples
-# 
+# @examples
 # f <- build_formula( "iGDP", list("iK", "iL", "iQP", "iYear")); f
 # g <- build_formula( "iGDP", c("iK", "iL", "iQP", "iYear")); g
 # identical(f,g)
@@ -59,44 +58,40 @@ replace_rhs <- function(formula, n, term, env=parent.frame()) {
 # keep_rhs(f, -(2:4))
 
 
-CDformulas <- 
-  list( 
-    PT = c(unadjusted = iGDP ~ iKstkS.L        + iL     + iXpMP + iYear,
-           adjusted = iGDP ~ iKservS.L       + ihLest + iUMP  + iYear),
-    UK = c(unadjusted = iGDP ~ iKserv0.WwithRD + iL     + iXp   + iYear,
-           adjusted = iGDP ~ iKserv0.WwithRD + ihLest + iU    + iYear)
-  )
-
-foo3 <- function(country=NULL, 
-                 flavor = c("unadjusted", "adjusted"), 
-                 include.energy = c(FALSE, TRUE),
-                 formulas = CDformulas){
-  
-  country <- numeric_choices(country, names(formulas))
-  
-  if (length(country) < 1) stop("No valid countries specified.")
-  
-  res <- list()  
-  for (c in names(country)) { 
-    for (f in flavor) {
-      for (ie in include.energy) {
-        new <- formulas[[c]][[f]]
-        if (!ie) new <- keep_rhs(new, -4)
-        res[[paste(c,f, if (ie) "energy" else "noenergy", sep=":")]] <- new
-      }
-    }
-  }
-  res
-}
+# CDformulas <- 
+#   list( 
+#     PT = c(unadjusted = iGDP ~ iKstkS.L        + iL     + iXpMP + iYear,
+#            adjusted = iGDP ~ iKservS.L       + ihLest + iUMP  + iYear),
+#     UK = c(unadjusted = iGDP ~ iKserv0.WwithRD + iL     + iXp   + iYear,
+#            adjusted = iGDP ~ iKserv0.WwithRD + ihLest + iU    + iYear)
+#   )
+# 
+# foo3 <- function(country=NULL, 
+#                  flavor = c("unadjusted", "adjusted"), 
+#                  include.energy = c(FALSE, TRUE),
+#                  formulas = CDformulas){
+#   
+#   country <- numeric_choices(country, names(formulas))
+#   
+#   if (length(country) < 1) stop("No valid countries specified.")
+#   
+#   res <- list()  
+#   for (c in names(country)) { 
+#     for (f in flavor) {
+#       for (ie in include.energy) {
+#         new <- formulas[[c]][[f]]
+#         if (!ie) new <- keep_rhs(new, -4)
+#         res[[paste(c,f, if (ie) "energy" else "noenergy", sep=":")]] <- new
+#       }
+#     }
+#   }
+#   res
+# }
 
 # foo3(country=1, include.energy=FALSE, flavor="adjusted")
-# 
 # foo3()
-# 
-# f <- iGDP ~ iK + iL + iQp + iYear
-# g <- replace_rhs( f, 3, paste(extract_rhs(f, 3), lhs(g), sep="/")); g
-# cdModel(g, data=subset(Calvin, Country="UK"))
-# 
+# f <- iGDP ~ iK + iL + iQp + iYear; f
+# g <- replace_rhs( f, 3, paste(extract_rhs(f, 3), lhs(f), sep="/")); g
+# cdModel(g, data=subset(Calvin, Country=="UK"))
 # 
 # 
-#   
