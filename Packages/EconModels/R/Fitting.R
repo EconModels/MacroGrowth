1c55bc964734f9ac41df0120b906a08055857ad6
MatthewHeun <matthew.heun@me.com>
2015-05-14 20:31:01 -0400

    Changed default constrained to TRUE for cdModel. Now consistent with cesModel.

--- Packages/EconModels/R/Fitting.R
+++ Packages/EconModels/R/Fitting.R
@@ -430,7 +430,7 @@ sfModel <- function(formula, data, response, factor, time, constrained=FALSE,
 #' 
 #' @export
 cdModel <- function(formula, data, response, x1, x2, x3, time, 
-                             constrained=FALSE, save.data=TRUE, ...) {
+                             constrained=TRUE, save.data=TRUE, ...) {
   if (missing(formula)) {
     if (missing(x3)) {
       return( cd2Model( data=data, 
