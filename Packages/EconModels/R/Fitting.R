b0c413350f7799fb4f90566aa9487c2d4d27f2fd
Randall Pruim <rpruim@gmail.com>
2015-06-11 12:31:44 +1200

    remove getData.lm temporarily

--- Packages/EconModels/R/Fitting.R
+++ Packages/EconModels/R/Fitting.R
@@ -217,14 +217,15 @@ getData.default <- function(object, ...) {
   object$data
 }
 
-#' @export
-getData.lm <- function(object, full = TRUE, ...) {
-  if (full) {
-    eval(object$call[["data"]], environment(object$call[["formula"]]))
-  } else {
-    model.frame(object, ...)
-  }
-}
+
+# #' @export
+# getData.lm <- function(object, full = TRUE, ...) {
+#   if (full) {
+#     eval(object$call[["data"]], environment(object$call[["formula"]]))
+#   } else {
+#     model.frame(object, ...)
+#   }
+# }
 
 
 #' Compute SSE from a model object
